26965853a73b1e3e7e71c12d46e8035ed586c3f8
Aditya Diwakar <aditya@diwakar.io>
2020-07-04 17:36:00 -0400

    Add BSPWMRC

---
+++ bspwm/layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-1 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate right --output DP-2 --mode 1920x1080 --pos 3640x0 --rotate left --output DP-3 --off --output HDMI-1 --mode 2560x1080 --pos 1080x634 --rotate normal
